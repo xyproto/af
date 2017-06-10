6ddd762cb6545ae7778d73be8e9ee40c9887ff6e
Alexander F Rødseth <rodseth@gmail.com>
2017-06-10 15:23:03 +0200

    Initial commit

---
+++ activation_test.go
@@ -0,0 +1,29 @@
+package activation
+
+import (
+	"fmt"
+	"math"
+	"testing"
+)
+
+func assertEqual(t *testing.T, a, b interface{}) {
+	if a == b {
+		return
+	}
+	t.Fatal(fmt.Sprintf("%v != %v", a, b))
+}
+
+func round(f float64) int {
+	if math.Abs(f) < 0.5 {
+		return 0
+	}
+	return int(f + math.Copysign(0.5, f))
+}
+
+func TestSigmoid(t *testing.T) {
+	assertEqual(t, Sigmoid(0.0), 0.5)
+}
+
+func TestTanh(t *testing.T) {
+	assertEqual(t, Tanh(0.0), 0.0)
+}
